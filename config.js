d7799cd626dcacc0afcea04d76d87a6cd7f62b60
Duccuteee <64661534+Duccutee@users.noreply.github.com>
2021-06-04 10:15:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Linh Nhi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Em nhà ở đâu đấy 😙',
+    desc: 'Cứ tới lui anh chẳng nhớ đường về ~~ ',
+    btnYes: 'Cứ khiến cho tôi ngày đêm phải khóc rồi cười vì nhớ một người <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu bé nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
